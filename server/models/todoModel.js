2b2907eaa9e4cfd2a35803ce50e6b699aaee3bbd
Verouge <branola1998628@gmail.com>
2023-11-02 21:01:25 +1100

    Create todoModel.js

---
+++ server/models/todoModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+  },
+  user: {
+    type: String,
+    required: [true, "User is required"],
+  },
+  date: {
+    type: String,
+  },
+  comments: {
+    type: [String],
+    default: [],
+  },
+  members: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
