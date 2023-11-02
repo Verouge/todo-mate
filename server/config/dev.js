c79f27bbb7cca4eb49ac12fe398e60520b1016b9
Verouge <branola1998628@gmail.com>
2023-11-02 21:02:00 +1100

    Create dev.js

---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://brandon:brandon1998@cluster0.ob4ovne.mongodb.net/?retryWrites=true&w=majority",
+  jwtSecret:
+    "kdjkasjqr8239r8kfjsajdnkqwd982398ry3fh297fy93f92ghfuvueibwniufvv2489y48939kdjkdjfkwt944039043kjgkwjgkwgwlkg404204wegklgkt42-owpglw4-y34pyo3py0239tfkeit049igkjkvjvw049t09tofjkwrjt034tigdfkjgkdfjg0rt9304fsdkfj04t904gigo",
+  jwtExpire: "24h",
+};
+
+// mongodb+srv://brandon:brandon1998@cluster0.ob4ovne.mongodb.net/?retryWrites=true&w=majority
